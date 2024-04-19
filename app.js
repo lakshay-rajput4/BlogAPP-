bfa620c3a327350811637c41598686da0b3b21ae
lakshay-rajput4 <137695779+lakshay-rajput4@users.noreply.github.com>
2024-04-19 23:40:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+//express app
+const app = express();
+
+//connect to mongodb
+const dbURI = 'mongodb+srv://lakshay4:node1234@cluster0.oqweari.mongodb.net/node1?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+//register view engines
+app.set('view engine', 'ejs');
+
+//middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));      //this takes all url encoded data and pass it on to the object for our use
+app.use(morgan('dev'));
+
+
+//routes
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+//blog routes   
+app.use('/blogs', blogRoutes);
+
+
+//404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
